366e84dcfdbae3396743137bc738458e35b30d4c
Ganesh Preetham Vulise <preethamvulise@gmail.com>
2025-07-24 23:50:06 +0530

    First commit

---
+++ submissions/finance_assistant/index.py
@@ -0,0 +1,3 @@
+
+def run_agent():
+    print("finance_assistant agent initialized.")
---
+++ submissions/finance_assistant/manifest.py
@@ -0,0 +1,8 @@
+
+manifest = {
+    "id": "finance_assistant",
+    "name": "Finance Assistant Agent",
+    "scopes": ["vault.read.email"],
+    "version": "0.1.0",
+    "description": "Generated agent for finance_assistant"
+}
